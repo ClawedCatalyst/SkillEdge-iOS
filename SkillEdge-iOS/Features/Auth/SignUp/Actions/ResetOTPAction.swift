3b822b49a0fc3ca0c0ff2a5fa0c0b9667a06cb4f
personal <suhaillahmadd0@gmail.com>
2023-11-29 21:15:46 +0530

    Fixed api calls in signup flow

--- SkillEdge-iOS/Features/Auth/SignUp/Actions/ResetOTPAction.swift
+++ SkillEdge-iOS/Features/Auth/SignUp/Actions/ResetOTPAction.swift
@@ -8,41 +8,30 @@
 import Foundation
 
 struct ResetOTPAction {
-    
+    let path = "/resend_otp/"
+    let method: HTTPMethod = .post
     var parameters: ResetOTPRequest
     
-    func call(completion: @escaping (ResetOTPResponse) -> Void) {
-        
-        let url = URL(string: "http://127.0.0.1:8000/api/resend_otp/")
-        
-        var request = URLRequest(url: url!)
-        request.httpMethod = "post"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        
-        do {
-            request.httpBody = try JSONEncoder().encode(parameters)
-        } catch {
-            // Error: Unable to encode request parameters
-        }
-        
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            if let data = data {
-                let response = try? JSONDecoder().decode(ResetOTPResponse.self, from: data)
-                
-                if let response = response {
-                    completion(response)
-                } else {
-                    // Error: Unable to decode response JSON
-                }
+    func call(
+        completion: @escaping (ResetOTPResponse) -> Void,
+        failure: @escaping (APIError) -> Void
+    ) {
+        APIRequest<ResetOTPRequest, ResetOTPResponse>.call(
+            path: path,
+            method: .post,
+            parameters: parameters
+        ) { data in
+            if let response = try? JSONDecoder().decode(
+                ResetOTPResponse.self,
+                from: data
+            ) {
+                completion(response)
             } else {
-                // Error: API request failed
-                if let error = error {
-                    print("Error: \(error.localizedDescription)")
-                }
+                failure(.jsonDecoding)
             }
+        }failure: { error in
+            failure(error)
         }
-        task.resume()
     }
 }
+
